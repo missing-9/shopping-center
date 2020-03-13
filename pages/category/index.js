a97278b5825435cdb66b0f089a12a3b836921354
missing-9 <qlgxljz@sina.com>
2020-03-14 00:48:04 +0800

    change style

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,5 +3,54 @@
 const app = getApp();
 
 Page({
+  data: {
+    category: [{
+        name: '热带辣果',
+        id: 'one'
+      },
+      {
+        name: '清新萌果',
+        id: 'two'
+      },
+      {
+        name: '留恋香果',
+        id: 'three'
+      },
+    ],
+    detail: [],
+    curIndex: 0,
+    isScroll: false,
+    toView: 'one'
+  },
+  onReady:{
+    wx.request({
+      url: '',
+      data: '',
+      header: {},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+  switchTab(e) {
+    const self = this;
+    this.setData({
+      isScroll: true
+    })
+    setTimeout(function () {
+      self.setData({
+        toView: e.target.dataset.id,
+        curIndex: e.target.dataset.index
+      })
+    }, 0)
+    setTimeout(function () {
+      self.setData({
+        isScroll: false
+      })
+    }, 1)
 
+  }
 });
