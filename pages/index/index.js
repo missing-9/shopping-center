e460672212c8186adaeb7a30857fb933fa8374ab
missing-9 <qlgxljz@sina.com>
2020-03-14 11:45:45 +0800

    goods

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,7 @@ Page({
   data: {
     announcement: 'Vant Weapp 是移动端 Vue 组件库 Vant 的小程序版本，两者基于相同的视觉规范，提供一致的 API 接口，助力开发者快速搭建小程序应用。',
     swiper: {
-      imgs: ['/static/img/shop/swiper/swiper-item0.jpg', '/static/img/shop/swiper/swiper-item1.jpg', '/static/img/shop/swiper/swiper-item2.jpg',
+      imgs: ['/static/img/shop/swiper/swiper-item1.jpg', '/static/img/shop/swiper/swiper-item2.jpg',
 '/static/img/shop/swiper/swiper-item3.jpg',],
       indicatorDots: true,
       vertical: false,
@@ -15,7 +15,7 @@ Page({
     grids: [
       { text: '快速购买', icon: '/static/img/shop/grids/purchase.png', url: './quickly-buy/index' },
       { text: '拼团', icon: '/static/img/shop/grids/group-buy.png', url: '' },
-      { text: '秒杀', icon: '/static/img/shop/grids/clock.png', url: '' },
+      { text: '秒杀', icon: '/static/img/shop/grids/clock.png', url: '../goods/index' },
       { text: '预约', icon: '/static/img/shop/grids/booking.png', url: '' },
       { text: '文章', icon: '/static/img/shop/grids/article.png', url: '' },
       { text: '视频', icon: '/static/img/shop/grids/vedio.png', url: '' },
