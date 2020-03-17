86e4fb6127c3190a70f9cadb7f6050aa127408dd
missing-9 <qlgxljz@sina.com>
2020-03-17 10:12:02 +0800

    add category page

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,41 +1,11 @@
 Page({
   data: {
-    category: [{
-        name: '热带辣果',
-        id: 'one'
-      },
-      {
-        name: '清新萌果',
-        id: 'two'
-      },
-      {
-        name: '留恋香果',
-        id: 'three'
-      },
-    ],
-    detail: [],
-    curIndex: 0,
-    isScroll: false,
-    toView: 'one',
-    kwd:' ',
-    loadingTxt: "加载更多"
+    activeKey: 0
   },
-  switchTab(e) {
-    const self = this;
-    this.setData({
-      isScroll: true
-    })
-    setTimeout(function () {
-      self.setData({
-        toView: e.target.dataset.id,
-        curIndex: e.target.dataset.index
-      })
-    }, 0)
-    setTimeout(function () {
-      self.setData({
-        isScroll: false
-      })
-    }, 1)
-
+  onChange(event) {
+    wx.showToast({
+      icon: 'none',
+      title: `切换至第${event.detail}项`
+    });
   }
 });
